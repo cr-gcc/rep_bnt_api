5779d425e85cc25f5eb84656e83800bf06002bb4
cris-contact-cinko <cris.gcc@outlook.com>
2025-12-30 16:20:15 -0600

    Nueva ip

--- config/cors.php
+++ config/cors.php
@@ -4,6 +4,7 @@ return [
 	'paths' => ['api/*', 'login', 'logout', 'me'],
 	'allowed_methods' => ['*'],
 	'allowed_origins' => [
+		'http://192.168.31.10',
 		'http://localhost:5173',
 	],
 	'allowed_origins_patterns' => [],
