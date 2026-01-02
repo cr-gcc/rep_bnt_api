06af797cc221358417e2d133ef61b820ff974aa3
cris-contact-cinko <cris.gcc@outlook.com>
2026-01-02 14:12:37 -0600

    Slug en status

--- database/seeders/StatusSeeder.php
+++ database/seeders/StatusSeeder.php
@@ -7,27 +7,28 @@ use Illuminate\Database\Seeder;
 
 class StatusSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
-    public function run(): void
-    {
-        $statuses = [
-            ['name' => 'Pendientes', 'code' => 0, 'slug' => 'pen'],
-            ['name' => 'Aprobadas', 'code' => 1, 'slug' => 'ap'],
-            ['name' => 'Rechazadas', 'code' => 2, 'slug' => 'nap'],
-            ['name' => 'Hold', 'code' => 3, 'slug' => 'hold'],
-            ['name' => 'Aprovadas AOP', 'code' => 4, 'slug' => 'aop'],
-            ['name' => 'Recuperadas', 'code' => 5, 'slug' => 'rec'],
-        ];
+	/**
+	 * Run the database seeds.
+	 */
+	public function run(): void
+	{
+		$statuses = [
+			['name' => 'Pendientes', 'code' => 0, 'slug' => 'pen'],
+			['name' => 'Aprobadas', 'code' => 1, 'slug' => 'ap'],
+			['name' => 'Rechazadas', 'code' => 2, 'slug' => 'nap'],
+			['name' => 'Hold', 'code' => 3, 'slug' => 'hold'],
+			['name' => 'Aprovadas AOP', 'code' => 4, 'slug' => 'aop'],
+			['name' => 'Recuperadas', 'code' => 5, 'slug' => 'rec'],
+		];
 
-        foreach ($statuses as $status) {
-            Status::firstOrCreate(
-                [
-									'name' => $status['name'],
-                  'code' => $status['code'],
-                ]
-            );
-        }
-    }
+		foreach ($statuses as $status) {
+			Status::firstOrCreate(
+				[
+					'name' => $status['name'],
+					'code' => $status['code'],
+					'slug' => $status['slug'],
+				]
+			);
+		}
+	}
 }
